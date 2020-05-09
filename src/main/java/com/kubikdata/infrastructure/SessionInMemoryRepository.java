e6dcb6b8807b2ed82e8b6673f5c4add47a8f2743
Ara <ara.manjoncb@gmail.com>
2020-05-10 01:54:47 +0200

    refactor(sessionInMemory): change caparity repository for only one userSession

--- src/main/java/com/kubikdata/infrastructure/SessionInMemoryRepository.java
+++ src/main/java/com/kubikdata/infrastructure/SessionInMemoryRepository.java
@@ -11,13 +11,13 @@ import java.util.List;
 @Component
 public class SessionInMemoryRepository implements Repository {
 
-  private final List<DTO.UserSession> userSessionList = new ArrayList<>();
+  private DTO.UserSession userSessionList;
 
   public DTO.UserSession findUser(Username username, Token token) {
-    return userSessionList.get(0);
+    return userSessionList;
   }
 
   public void addUser(DTO.UserSession userSession) {
-    this.userSessionList.add(userSession);
+    this.userSessionList = userSession;
   }
 }

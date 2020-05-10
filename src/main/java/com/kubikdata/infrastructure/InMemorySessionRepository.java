47cc739e7dc36a98dac2959a4eab547735373371
Ara <ara.manjoncb@gmail.com>
2020-05-10 22:45:31 +0200

    refactor(inMemorySessionRepository): add two filters to find userSession with same username and token

--- src/main/java/com/kubikdata/infrastructure/InMemorySessionRepository.java
+++ src/main/java/com/kubikdata/infrastructure/InMemorySessionRepository.java
@@ -8,16 +8,20 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 @Component
 public class InMemorySessionRepository implements Repository {
 
-  private Collection<DTO.UserSession> userSessionList = new ArrayList<>();
+  private final Collection<DTO.UserSession> userSessionList = new ArrayList<>();
 
   public Optional<DTO.UserSession> findUser(Username username, Token token) {
 
+    Predicate<DTO.UserSession> isSameUsername = userSession -> userSession.username.equals(username.getUsername());
+    Predicate<DTO.UserSession> isSameToken = userSession -> userSession.token.equals(token.getToken());
+
     return this.userSessionList.stream()
-        .filter(userSession -> userSession.username.equals(username.getUsername()))
+        .filter(isSameUsername.and(isSameToken))
         .findFirst();
   }
 

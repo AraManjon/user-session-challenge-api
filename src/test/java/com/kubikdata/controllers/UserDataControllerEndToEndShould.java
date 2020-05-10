2b961d7264996603461f0040cc7dc7382a0c8b89
Ara <ara.manjoncb@gmail.com>
2020-05-10 16:54:33 +0200

    test(userDataControllerEndToEnd)-green: find user data in inMemorySessionRepository correctly

--- src/test/java/com/kubikdata/controllers/UserDataControllerEndToEndShould.java
+++ src/test/java/com/kubikdata/controllers/UserDataControllerEndToEndShould.java
@@ -3,31 +3,38 @@ package com.kubikdata.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kubikdata.controllers.request.UserSessionRequest;
 import com.kubikdata.controllers.response.SessionResponse;
+import com.kubikdata.controllers.response.UserResponse;
 import com.kubikdata.infrastructure.Repository;
 import com.kubikdata.services.TimeServer;
 import com.kubikdata.services.TokenGenerator;
 import com.kubikdata.services.TokenUsernameGenerator;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Date;
 import java.util.Random;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class UserSessionControllerEndToEndShould {
+public class UserDataControllerEndToEndShould {
 
   @Autowired
   private MockMvc mockMvc;
@@ -36,30 +43,47 @@ public class UserSessionControllerEndToEndShould {
   private ObjectMapper objectMapper;
 
   @Autowired
-  private TokenGenerator tokenUsernameGenerator;
-
-  @Autowired
-  private TimeServer timeDataServer;
-
-  @Autowired
   private Repository sessionInMemoryRepository;
 
-  @Test
-  public void create_a_token_when_add_a_session_correctly() throws Exception {
+  @MockBean
+  private TokenGenerator tokenUsernameGenerator;
 
-    String username = new Random().toString();
+  @MockBean
+  private TimeServer timeDataServer;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  private void createDummyRepository(String username, String token, Date date) throws Exception {
+
     UserSessionRequest userSessionRequest = new UserSessionRequest();
     userSessionRequest.setUsername(username);
-    TokenUsernameGenerator generator = new TokenUsernameGenerator();
-    String jsonRequest = new ObjectMapper().writeValueAsString(userSessionRequest);
+    when(timeDataServer.generate()).thenReturn(date);
+    when(tokenUsernameGenerator.code(username)).thenReturn(token);
 
-    MvcResult result = this.mockMvc.perform(post("/session").contentType(MediaType.APPLICATION_JSON)
-        .content(jsonRequest)).andDo(print()).andExpect(status().is(200))
+    this.mockMvc.perform(post("/session").contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(userSessionRequest))).andDo(print()).andExpect(status().is(200))
+        .andReturn();
+  }
+
+  @Test
+  public void find_user_data_correctly() throws Exception {
+
+    String username = new Random().toString();
+    Date date = new Date();
+    String token = "token";
+    createDummyRepository(username, token, date);
+    UserResponse userResponseExpected = new UserResponse(username, token, date);
+
+    MvcResult result = this.mockMvc.perform(get("/info/"+username+"/"+token))
+        .andDo(print()).andExpect(status().is(200))
         .andReturn();
     String resultAsString = result.getResponse().getContentAsString();
-    SessionResponse sessionResponse = objectMapper.readValue(resultAsString, SessionResponse.class);
+    UserResponse userResponse = objectMapper.readValue(resultAsString, UserResponse.class);
 
     Assertions.assertNotNull(resultAsString);
-    Assertions.assertEquals(username, generator.decode(sessionResponse.getToken()).getSubject());
+    Assertions.assertEquals(userResponseExpected, userResponse);
   }
 }

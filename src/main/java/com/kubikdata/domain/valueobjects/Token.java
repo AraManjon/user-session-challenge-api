a8c551719edaddeca634fd3d9dfd482ba1a7bad9
Ara <ara.manjoncb@gmail.com>
2020-05-10 19:29:28 +0200

    feat(userSessionController): should throw an error when token is empty

--- src/main/java/com/kubikdata/domain/valueobjects/Token.java
+++ src/main/java/com/kubikdata/domain/valueobjects/Token.java
@@ -4,6 +4,7 @@ public class Token {
   private final String token;
 
   public Token(String token) {
+    if(token.isEmpty()) throw new RuntimeException("Token can not be empty");
     this.token = token;
   }
 

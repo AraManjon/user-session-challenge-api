6b854200964cde201d2662af536462bcbdf359f4
Ara <ara.manjoncb@gmail.com>
2020-05-18 13:55:43 +0200

    changes in configuration

--- src/main/java/com/usersession/configuration/SwaggerConfiguration.java
+++ src/main/java/com/usersession/configuration/SwaggerConfiguration.java
@@ -20,7 +20,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.kubikdata.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.usersession.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
